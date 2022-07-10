cb90a7818eeb03a11e2f5d67f002fb4edaed778d
SandroMaglione <lass.maglio@gmail.com>
2022-07-10 10:48:40 +0200

    search+highlight any element in the page

--- contents/index.ts
+++ contents/index.ts
@@ -1,9 +1,70 @@
+import { nanoid } from "nanoid"
+
 import {
   mapColorToWord,
   pickTextColorBasedOnBgColorAdvanced
 } from "~lib/colors"
-import { TAG_CLASS } from "~lib/constants"
+import { OVERVIEW_ID, TAG_CLASS } from "~lib/constants"
 import { getStorage, upsertStorage } from "~lib/storage"
+
+type FoundWord = {
+  id: string
+  word: string
+}
+
+const applyHighlight =
+  (
+    searchRegex: RegExp,
+    colorMap: Map<string, string>,
+    foundWords: FoundWord[]
+  ) =>
+  (element: Element): FoundWord[] => {
+    if (element.id === OVERVIEW_ID) {
+      return foundWords
+    }
+
+    // Leaf node (no children)
+    if (element.children.length === 0) {
+      // Not already highlighted
+      if (!element.classList.contains(TAG_CLASS)) {
+        const text = element.textContent
+        const textMatch = text?.match(searchRegex)
+
+        // Has text with matching word in regex (word list)
+        if (text && (textMatch?.length ?? 0) > 0) {
+          const wordIds = Array.from(textMatch ?? [], () => nanoid())
+          let wordIndexId = 0
+
+          const highlight = text.replace(searchRegex, (str) => {
+            const wordId = wordIds[wordIndexId]
+            wordIndexId = wordIndexId + 1
+
+            const bgColor = colorMap.get(str) ?? "#000080"
+            return `<a href="https://jpdb.io/search?q=${str}" target="_blank" rel="noopener noreferrer" id="${wordId}" class="${TAG_CLASS}" style="font-weight:300;background:${bgColor};color:${pickTextColorBasedOnBgColorAdvanced(
+              bgColor,
+              "#fff",
+              "#111"
+            )};scroll-margin-top: 60px;padding:3px 4px;text-decoration:none;">${str}</a>`
+          })
+
+          element.innerHTML = highlight
+          return [
+            ...foundWords,
+            ...(textMatch ?? []).map((matchWord, index) => ({
+              word: matchWord,
+              id: wordIds[index]
+            }))
+          ]
+        }
+      }
+
+      return foundWords
+    } else {
+      return Array.from(element.children)
+        .map(applyHighlight(searchRegex, colorMap, foundWords))
+        .flat()
+    }
+  }
 
 const highlightAll = async () => {
   const data = await getStorage()()
@@ -15,35 +76,35 @@ const highlightAll = async () => {
       e1.length > e2.length ? -1 : e2.length > e1.length ? 1 : 0
     )
 
-    document.querySelectorAll(`.${TAG_CLASS}`).forEach((e) => {
-      e.replaceWith(e.textContent ?? "")
-    })
+    const regex = sortByLength.join("|")
+    const foundWords = applyHighlight(
+      new RegExp(`${regex}`, "g"),
+      colorMap,
+      []
+    )(document.body)
 
-    document.querySelectorAll("p, span").forEach((e) => {
-      const text = e.textContent
-      if (text) {
-        console.log({ text })
+    if (foundWords.length > 0) {
+      const element =
+        document.getElementById(OVERVIEW_ID) ?? document.createElement("p")
 
-        const regex = sortByLength.join("|")
-        const highlight = text.replace(new RegExp(`${regex}`, "g"), (str) => {
-          const bgColor = colorMap.get(str) ?? "#000080"
-          return `<a href="https://jpdb.io/search?q=${str}" target="_blank" rel="noopener noreferrer" class="${TAG_CLASS}" style="font-weight:300;background:${bgColor};color:${pickTextColorBasedOnBgColorAdvanced(
-            bgColor,
-            "#fff",
-            "#111"
-          )};padding:3px 4px;text-decoration:none;">${str}</a>`
-        })
+      foundWords.forEach(({ id, word }) => {
+        const a = document.createElement("a")
+        a.href = `#${id}`
+        a.textContent = word
+        element.appendChild(a)
+      })
 
-        e.innerHTML = highlight
-      }
-    })
+      element.classList.add(`${OVERVIEW_ID}`)
+      element.id = `${OVERVIEW_ID}`
+      document.body.appendChild(element)
+
+      console.log({ foundWords })
+    }
   }
 }
 
 document.onkeydown = async (e) => {
   if (e.altKey && e.ctrlKey) {
-    console.log({ key: e.key })
-
     if (e.key === "Dead") {
       /** Highlight new word (Alt + Ctrl + N) */
       const text = document.getSelection()
@@ -54,7 +115,7 @@ document.onkeydown = async (e) => {
         highlightAll()
       }
     } else if (e.key === "µ") {
-      /** Reload word (for SPA apps without SSR) (Alt + Ctrl + M) */
+      /** Reload words (for SPA apps without SSR) (Alt + Ctrl + M) */
       highlightAll()
     }
   }
@@ -62,7 +123,10 @@ document.onkeydown = async (e) => {
 
 /** Apply css stylesheet */
 const style = document.createElement("style")
-style.textContent = `.${TAG_CLASS}:hover { filter: brightness(1.25); }`
+style.textContent = `.${OVERVIEW_ID} { display: flex; flex-wrap: wrap; column-gap: 6px; max-width: 33vw; position: fixed; bottom: 0; right: 0; padding: 12px 16px; background: white; border-radius: 8px 0 0 0; border: 1px solid rgb(125 211 252); } .${TAG_CLASS}:hover { filter: brightness(1.25); }`
 document.head.appendChild(style)
 
 highlightAll()
+setTimeout(() => {
+  highlightAll()
+}, 3000)
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,2 @@
-export const TAG_CLASS = "h-gl"
+export const TAG_CLASS = "055a7993-72a9-4c93-abdb-a445c37f12a6"
+export const OVERVIEW_ID = `overview-${TAG_CLASS}` as const
