88d6e15843396a02d3a967dbcd7994a452c80c62
SandroMaglione <lass.maglio@gmail.com>
2022-11-09 18:15:32 +0100

    add word from popup near selection

--- contents/index.ts
+++ contents/index.ts
@@ -6,14 +6,22 @@ import {
   mapColorToWord,
   pickTextColorBasedOnBgColorAdvanced
 } from "~lib/colors"
-import { OVERVIEW_ID, TAG_CLASS } from "~lib/constants"
+import { OVERVIEW_ID, POPUP_ID, TAG_CLASS } from "~lib/constants"
 import { getStorage, upsertStorage } from "~lib/storage"
+
+let mouseX: number | null = null
+let mouseY: number | null = null
 
 const storage = new Storage()
 
 type FoundWord = {
   id: string
   word: string
+}
+
+const saveSelection = async (selection: string) => {
+  await upsertStorage(selection)()
+  highlightAll()
 }
 
 /**
@@ -143,35 +151,27 @@ const highlightAll = async () => {
       []
     )(document.body)
 
-    if (foundWords.length > 0) {
-      const element =
-        document.getElementById(OVERVIEW_ID) ?? document.createElement("p")
+    // if (foundWords.length > 0) {
+    //   const element =
+    //     document.getElementById(OVERVIEW_ID) ?? document.createElement("p")
 
-      foundWords.forEach(({ id, word }) => {
-        const a = document.createElement("a")
-        a.href = `#${id}`
-        a.textContent = word
-        element.appendChild(a)
-      })
+    //   foundWords.forEach(({ id, word }) => {
+    //     const a = document.createElement("a")
+    //     a.href = `#${id}`
+    //     a.textContent = word
+    //     element.appendChild(a)
+    //   })
 
-      element.classList.add(`${OVERVIEW_ID}`)
-      element.id = `${OVERVIEW_ID}`
-      document.body.appendChild(element)
-    }
+    //   element.classList.add(`${OVERVIEW_ID}`)
+    //   element.id = `${OVERVIEW_ID}`
+    //   document.body.appendChild(element)
+    // }
   }
 }
 
 document.onkeydown = async (e) => {
   if (e.altKey && e.ctrlKey) {
-    if (e.key === "Dead") {
-      /** Highlight new word (Alt + Ctrl + N) */
-      const text = document.getSelection()
-      if (text !== null && text.rangeCount > 0) {
-        const selection = text.getRangeAt(0).toString()
-        await upsertStorage(selection)()
-        highlightAll()
-      }
-    } else if (e.key === "µ") {
+    if (e.key === "µ") {
       /** Reload words (for SPA apps without SSR) (Alt + Ctrl + M) */
       highlightAll()
     }
@@ -180,31 +180,56 @@ document.onkeydown = async (e) => {
 
 /** Apply css stylesheet */
 const style = document.createElement("style")
-style.textContent = `.${OVERVIEW_ID} { display: flex; flex-wrap: wrap; column-gap: 6px; max-width: 33vw; position: fixed; bottom: 0; right: 0; padding: 12px 16px; background: white; border-radius: 8px 0 0 0; border: 1px solid rgb(125 211 252); } .${TAG_CLASS}:hover { filter: brightness(1.25); }`
+style.appendChild(
+  document.createTextNode(`
+#${POPUP_ID} { position: absolute; padding: 12px 16px; background: #f9fafb; border-radius: 8px; border: 1px solid #fccebd; }
+#${POPUP_ID}:hover { cursor: pointer; background: #fff; }
+  
+.${OVERVIEW_ID} { display: flex; flex-wrap: wrap; column-gap: 6px; max-width: 33vw; position: fixed; bottom: 0; right: 0; padding: 12px 16px; background: white; border-radius: 8px 0 0 0; border: 1px solid rgb(125 211 252); }
+.${TAG_CLASS}:hover { filter: brightness(1.25); }
+`)
+)
 document.head.appendChild(style)
 
 highlightAll()
-document.addEventListener("selectionchange", async (event) => {
-  const data = await storage.get("a")
+
+document.addEventListener("mousemove", (event) => {
+  mouseX = event.pageX
+  mouseY = event.pageY
+})
+
+document.addEventListener("selectionchange", async () => {
   const selection = document.getSelection()
+
   const focusNode = selection?.focusNode
   const parent = focusNode?.parentElement
-  if (parent) {
-    const { top, right } = parent.getBoundingClientRect()
-    const newDiv = document.createElement("div")
-    newDiv.style.position = "fixed"
-    newDiv.style.top = `${top}px`
-    newDiv.style.right = `${right}px`
-    newDiv.style.width = "100px"
-    newDiv.style.height = "100px"
-    newDiv.style.background = "#e3e3e3"
+  const selected = selection?.toString() ?? ""
 
-    parent.appendChild(newDiv)
+  if (selected.length === 0) {
+    setTimeout(() => {
+      const prevElement = document.getElementById(POPUP_ID)
+      if (prevElement !== null) {
+        prevElement.remove()
+      }
+    }, 150)
   }
 
-  const selected = selection?.toString() ?? ""
-  if (selected.length > 0) {
-    alert(selection?.toString())
+  if (parent && selected.length > 0) {
+    const prevElement = document.getElementById(POPUP_ID)
+    if (prevElement !== null) {
+      prevElement.remove()
+    }
+
+    const newDiv = document.createElement("div")
+    newDiv.id = `${POPUP_ID}`
+    newDiv.style.top = `${(mouseY ?? 0) + 16}px`
+    newDiv.style.left = `${(mouseX ?? 0) - 8}px`
+    newDiv.textContent = `${selected}`
+    newDiv.addEventListener("click", async () => {
+      await saveSelection(selected)
+    })
+
+    document.body.appendChild(newDiv)
   }
 })
 
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,3 @@
-export const TAG_CLASS = "055a7993-72a9-4c93-abdb-a445c37f12a6"
+export const TAG_CLASS = "tag-055a7993-72a9-4c93-abdb-a445c37f12a6"
+export const POPUP_ID = "popup-450f973d-c843-4cce-9352-89722af6d691"
 export const OVERVIEW_ID = `overview-${TAG_CLASS}` as const
